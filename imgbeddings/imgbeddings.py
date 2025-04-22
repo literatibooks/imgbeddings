aa92c0f409d2ed43738151755176bab5172297a6
Matthew A Swain <10989+mswain@users.noreply.github.com>
2025-04-22 17:52:07 -0500

    HuggingFace Hub API updated, build tooling migrated to uv

--- imgbeddings/imgbeddings.py
+++ imgbeddings/imgbeddings.py
@@ -4,7 +4,7 @@ import random
 import itertools
 
 from transformers import CLIPProcessor
-from huggingface_hub import hf_hub_url, cached_download
+from huggingface_hub import hf_hub_url, hf_hub_download
 from onnxruntime import InferenceSession
 import numpy as np
 from tqdm.auto import tqdm
@@ -34,12 +34,9 @@ class imgbeddings:
 
         if self.model_path is None:
             model_filename = f"patch{self.patch_size}_v{self.version}.onnx"
-
-            config_file_url = hf_hub_url(
-                repo_id="minimaxir/imgbeddings", filename=model_filename
-            )
-            self.model_path = cached_download(
-                config_file_url, force_filename=model_filename
+            self.model_path = hf_hub_download(
+                repo_id="minimaxir/imgbeddings",
+                filename=model_filename
             )
 
         self.session = create_session_for_provider(
